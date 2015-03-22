96485f5fbd73a0710b822f2ef2fa2a5c4f114e85
Ken Lin <github.kenklin@kenlin.com>
2015-03-21 20:13:36 -0400

    Fix activity_ids values; add subject_ids

--- run_analysis.R
+++ run_analysis.R
@@ -14,11 +14,24 @@ run_analysis <- function() {
   
   train           <- read.table("X_train.txt")
   ytrain          <- read.table("y_train.txt")
+  subject_train   <- read.table("subject_train.txt")
+
   test            <- read.table("X_test.txt")
   ytest           <- read.table("y_test.txt")
+  subject_test    <- read.table("subject_test.txt")
 
   # 1. Merge the training and the test sets to create one data set.
   data <- rbind(train, test)
+
+  # 1a. Append activity_ids column (numeric ids might be use)
+  newcol <- rbind(ytrain, ytest)
+  feature_labels <- c(feature_labels, "activity_ids")
+  data$activity_ids <- newcol[,1]
+  
+  # 1b. Append subject_ids column
+  newcol <- rbind(subject_train, subject_test)
+  feature_labels <- c(feature_labels, "subject_ids")
+  data$subject_ids <- newcol[,1]
   
   # 2. Extract only the measurements on the mean and standard deviation for each
   #    measurement.  Per features_info.txt ...
@@ -30,21 +43,18 @@ run_analysis <- function() {
   extracteddata <- data[, colindices]
 
   # 3. Use descriptive activity names to name the activities in the data set
-  activity_ids <- rbind(ytrain, ytest)
-  data$activity_ids <- activity_ids[1,]
-  feature_labels <- c(feature_labels, "activity_ids")
   
   # 4. Appropriately labels the data set with descriptive variable names.
-  feature_labels <- gsub("-",       "_", feature_labels)
-  feature_labels <- gsub(",",       "_", feature_labels)
-  #  feature_labels <- gsub("\\(\\)$", "",  feature_labels)
-  feature_labels <- gsub("\\(",     "", feature_labels)
-  feature_labels <- gsub("\\)",     "",  feature_labels)
-  feature_labels <- gsub("__",      "_", feature_labels)
+  feature_labels <- gsub("-",   "_", feature_labels)
+  feature_labels <- gsub(",",   "_", feature_labels)
+  feature_labels <- gsub("\\(", "",  feature_labels)
+  feature_labels <- gsub("\\)", "",  feature_labels)
+  feature_labels <- gsub("__",  "_", feature_labels)
   
   names(data) <- feature_labels
 
   # 5. From the data set in step 4, creates a second, independent tidy data set
   #    with the average of each variable for each activity and each subject.
   
+  data
 }
