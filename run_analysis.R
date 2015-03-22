fe2b344ead5fe0c133f7934d1e1472ada6db775a
Ken Lin <github.kenklin@kenlin.com>
2015-03-22 15:52:33 -0400

    Beginning of group_by

--- run_analysis.R
+++ run_analysis.R
@@ -19,39 +19,43 @@ run_analysis <- function() {
   # 1. Merge the training and the test sets to create one data set.
   data <- rbind(train, test)
 
-  # 1a. Append activity_ids column (numeric ids might be use)
-  newcol <- rbind(ytrain, ytest)
-  vFeatureLabels <- c(vFeatureLabels, "activity_ids")
-  data$activity_ids <- newcol[,1]
-  
-  # 1b. Append subject_ids column
-  newcol <- rbind(subject_train, subject_test)
-  vFeatureLabels <- c(vFeatureLabels, "subject_ids")
-  data$subject_ids <- newcol[,1]
-  
-  # 2. Extract only the measurements on the mean and standard deviation for each
+  # 2. Appropriately label the data set with descriptive variable names.
+  vFeatureLabels <- gsub("-",   "_", vFeatureLabels)
+  vFeatureLabels <- gsub(",",   "_", vFeatureLabels)
+  vFeatureLabels <- gsub("\\(", "",  vFeatureLabels)
+  vFeatureLabels <- gsub("\\)", "",  vFeatureLabels)
+  vFeatureLabels <- gsub("__",  "_", vFeatureLabels)
+  names(data) <- vFeatureLabels
+    
+  # 3. Extract only the measurements on the mean and standard deviation for each
   #    measurement.  Per features_info.txt ...
   #
   #        The set of variables that were estimated from these signals are:
   #             mean(): Mean value
   #             std(): Standard deviation
   colindices <- features[grep("mean\\(\\)|std\\(\\)", features$V2), 1]
-  extracteddata <- data[, colindices]
+  data <- data[, colindices]
+  
+  # 3a. Append activity_ids column (numeric ids might be use)
+  newcol <- rbind(ytrain, ytest)
+  data$activity_ids <- newcol[,1]
+  
+  # 3b. Append subject_ids column
+  newcol <- rbind(subject_train, subject_test)
+  data$subject_ids <- newcol[,1]
 
-  # 3. Use descriptive activity names to name the activities in the data set
+  # 4. Use descriptive activity names to name the activities in the data set
   data <- merge(data, dfActivityLabels, by="activity_ids")
-  
-  # 4. Appropriately labels the data set with descriptive variable names.
-  vFeatureLabels <- gsub("-",   "_", vFeatureLabels)
-  vFeatureLabels <- gsub(",",   "_", vFeatureLabels)
-  vFeatureLabels <- gsub("\\(", "",  vFeatureLabels)
-  vFeatureLabels <- gsub("\\)", "",  vFeatureLabels)
-  vFeatureLabels <- gsub("__",  "_", vFeatureLabels)
-  
-  names(data) <- vFeatureLabels
 
   # 5. From the data set in step 4, creates a second, independent tidy data set
   #    with the average of each variable for each activity and each subject.
   
-  data
+  # group_by, summarize_each per https://class.coursera.org/getdata-012/forum/thread?thread_id=9#comment-792
+  groupedData <- group_by(data, activity_names, subject_ids)
+  tidy <- summarize(groupedData, mean=mean(fBodyBodyGyroJerkMag_mean))
+  
+  # Write data to tidydata.txt in working directory
+#  write.table(data, file = "tidydata.txt", row.name = FALSE)
+
+  tidy
 }
